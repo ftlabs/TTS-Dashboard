2e37b1c7db841551619c2cad2f57d838e798757e
Sqyphen <tobias.fieldhouse@gmail.com>
2020-03-24 11:41:50 +0000

    inital changes to get OKTA setup

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const authS3O = require('@financial-times/s3o-middleware');
+//const authS3O = require('@financial-times/s3o-middleware');
+const session = require('cookie-session');
+const OktaMiddleware = require('@financial-times/okta-express-middleware');
+
+const okta = new OktaMiddleware({
+  client_id: process.env.OKTA_CLIENT,
+  client_secret: process.env.OKTA_SECRET,
+  issuer: process.env.OKTA_ISSUER,
+  appBaseUrl: process.env.BASE_URL,
+  scope: 'openid offline_access name'
+});
+
+router.use(session({
+	secret: process.env.SESSION_TOKEN,
+	maxAge: 24 * 3600 * 1000, //24h
+	httpOnly: true
+}));
 
 const services = require('../bin/lib/list-services');
 
 /* GET home page. */
-router.get('/', authS3O,function(req, res, next) {
+router.get('/',function(req, res, next) {
+
+  router.use(okta.router);
+  router.use(okta.ensureAuthenticated());
+  router.use(okta.verifyJwts());
+
   res.render('index', { 
     title: process.env.SERVICE_NAME || 'FT Labs TTS Dashboard',
     serviceName: process.env.SERVICE_NAME || 'FT Labs TTS Dashboard',
